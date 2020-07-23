06432b2e5461589cc3ec4393625f91c98ccd6801
Paddywaan <paddy@bitnibbler.co.uk>
2020-07-23 18:10:52 +0100

    Add project files.

---
+++ HandbrakeCLI-daemon/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HandbrakeCLI_daemon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

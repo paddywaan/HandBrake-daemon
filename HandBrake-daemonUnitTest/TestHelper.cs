a570115a39c23de6a5ad82b6ca368136252dbcb5
Paddy <paddy@bitnibbler.co.uk>
2020-08-28 10:35:51 +0100

    Tidied up unit tests, implemented assembly info & versions

---
+++ HandBrake-daemonUnitTest/TestHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace HandbrakeCLI_daemonUnitTest
+{
+    static class TestHelper
+    {
+        public static string ASMDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        public static string TESTDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + Path.DirectorySeparatorChar + "TestMedia" + Path.DirectorySeparatorChar;
+        public static void FileCreate(string path)
+        {
+            System.IO.FileInfo file = new System.IO.FileInfo(ASMDir + Path.DirectorySeparatorChar + "TestMedia" + Path.DirectorySeparatorChar + path);
+            file.Directory.Create();
+            File.Create(file.FullName);
+        }
+    }
+}
--- HandBrake-daemonUnitTest/TestQueue.cs
+++ HandBrake-daemonUnitTest/TestQueue.cs
@@ -4,23 +4,19 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
+using static HandbrakeCLI_daemonUnitTest.TestHelper;
 
 namespace HandbrakeCLI_daemonUnitTest
 {
     public class TestQueue
     {
         List<string> testRegexStrings;
-        string asmDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-        string testDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + Path.DirectorySeparatorChar + "TestMedia" + Path.DirectorySeparatorChar;
-        //List<string> testMedia = new List<string> { "test", "test" + Path.DirectorySeparatorChar + "testing" };
-        //List<string> testnumberedsrt = new List<string> { "2_eng.srt", "4_fre.srt" };
-        //List<string> testmedianamesrt = new List<string> { ".eng.srt", ".fre.srt", ".(2_English).srt"};
 
         [TearDown]
         public void TearDown()
         {
             GC.Collect();
-            var x = asmDir + Path.DirectorySeparatorChar + "TestMedia" + Path.DirectorySeparatorChar;
+            var x = ASMDir + Path.DirectorySeparatorChar + "TestMedia" + Path.DirectorySeparatorChar;
             if (Directory.Exists(x)) Directory.Delete(x, true);
         }
 
@@ -62,14 +58,14 @@ namespace HandbrakeCLI_daemonUnitTest
         {
             FileCreate("testMedia.mp4");
             FileCreate("testMedia.srt");
-            Assert.AreEqual("und", QueueService.GetSubs(testDir + "testMedia.mp4").Item2[0]);
+            Assert.AreEqual("und", QueueService.GetSubs(TESTDir + "testMedia.mp4").Item2[0]);
         }
         [Test]
         public void TestFNameContainsLangReturnsLang()
         {
             FileCreate("testMedia.mp4");
             FileCreate("testMedia.English.srt");
-            Assert.AreEqual("English", QueueService.GetSubs(testDir + "testMedia.mp4").Item2[0]);
+            Assert.AreEqual("English", QueueService.GetSubs(TESTDir + "testMedia.mp4").Item2[0]);
         }
         [Test]
         public void TestLangsForSRTNamesOnly()
@@ -77,8 +73,8 @@ namespace HandbrakeCLI_daemonUnitTest
             FileCreate("testMedia.mp4");
             FileCreate("Subs\\2_English.srt");
             FileCreate("Subs\\3_French.srt");
-            Assert.AreEqual("English", QueueService.GetSubs(testDir + "testMedia.mp4").Item2[0]);
-            Assert.AreEqual("French", QueueService.GetSubs(testDir + "testMedia.mp4").Item2[1]);
+            Assert.AreEqual("English", QueueService.GetSubs(TESTDir + "testMedia.mp4").Item2[0]);
+            Assert.AreEqual("French", QueueService.GetSubs(TESTDir + "testMedia.mp4").Item2[1]);
         }
         [Test]
         public void TestIdentialNameInSubsDirectory()
@@ -87,28 +83,30 @@ namespace HandbrakeCLI_daemonUnitTest
             FileCreate("Subs\\testMediaOne.srt");
             FileCreate("testMediaTwo.mp4");
             FileCreate("Subs\\testMediaTwo.srt");
-            Assert.AreEqual("und", QueueService.GetSubs(testDir + "testMediaOne.mp4").Item2[0]);
-            Assert.AreEqual(1, QueueService.GetSubs(testDir + "testMediaOne.mp4").Item2.Count);
-            Assert.AreEqual("und", QueueService.GetSubs(testDir + "testMediaTwo.mp4").Item2[0]);
-            Assert.AreEqual(1, QueueService.GetSubs(testDir + "testMediaTwo.mp4").Item2.Count);
+            Assert.AreEqual("und", QueueService.GetSubs(TESTDir + "testMediaOne.mp4").Item2[0]);
+            Assert.AreEqual(1, QueueService.GetSubs(TESTDir + "testMediaOne.mp4").Item2.Count);
+            Assert.AreEqual("und", QueueService.GetSubs(TESTDir + "testMediaTwo.mp4").Item2[0]);
+            Assert.AreEqual(1, QueueService.GetSubs(TESTDir + "testMediaTwo.mp4").Item2.Count);
         }
+        /// <summary>
+        /// Required for non NTFS
+        /// </summary>
         [Test]
-        public void testTorSubsNotAdded()
+        public void TestUpperCaseSensitiveDirectory()
         {
-            FileCreate("the.expanse.s01e04.1080p.bluray.x264-rovers.mkv");
-            FileCreate("Subs\\The.Expanse.S01E04.1080p.BluRay.x264-ROVERS.srt");
-            Assert.AreEqual("und", QueueService.GetSubs(testDir + "the.expanse.s01e04.1080p.bluray.x264-rovers.mkv").Item2[0]);
+            FileCreate("testMedia.mp4");
+            FileCreate("Subs\\testMedia.srt");
+            Assert.AreEqual("und", QueueService.GetSubs(TESTDir + "testMedia.mp4").Item2[0]);
         }
-        private void FileCreate(string path)
+        /// <summary>
+        /// Required for non NTFS
+        /// </summary>
+        [Test]
+        public void TestLowerCaseSensitiveDirectory()
         {
-            System.IO.FileInfo file = new System.IO.FileInfo(asmDir + Path.DirectorySeparatorChar + "TestMedia" + Path.DirectorySeparatorChar + path);
-            file.Directory.Create();
-            File.Create(file.FullName);
+            FileCreate("testMedia.mp4");
+            FileCreate("subs\\testMedia.srt");
+            Assert.AreEqual("und", QueueService.GetSubs(TESTDir + "testMedia.mp4").Item2[0]);
         }
-
-    }
-    public class TestSRT
-    {
-        
     }
 }

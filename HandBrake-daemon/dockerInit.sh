bc2a5bf50c36b0ce92fbe5175699097ea15a78c8
paddywaan <paddy@bitnibbler.co.uk>
2020-10-18 12:37:45 +0100

    Made dockerINit easier for first time users to configure

--- HandBrake-daemon/dockerInit.sh
+++ HandBrake-daemon/dockerInit.sh
@@ -6,6 +6,8 @@ if [[ ! -f /etc/"$conf" ]]; then #This is first run.
     echo Initialized watcher
     if [[ ! -f "$mountdir"/handbrake-daemon.conf ]]; then #Init was not called before, therefore we use blank config.
         cp default.conf "$mountdir"/handbrake-daemon.conf
+        sed -i -e 's/media/handbrake/g' -e 's/transcoding\///g' "$mountdir"/handbrake-daemon.conf
+        mkdir "$mountdir"/sourceDirectory "$mountdir"/completedTranscodes "$mountdir"/originalMedia
     fi
 fi
 cp "$mountdir"/handbrake-daemon.conf /etc/handbrake-daemon.conf #Move the hostfile to take effect inside the container.

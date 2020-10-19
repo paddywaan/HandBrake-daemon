9f69c3949773d38462ae46ab3760682d6335867b
paddywaan <paddy@bitnibbler.co.uk>
2020-10-19 15:10:04 +0100

    Recovered from IDE madness, applied few fixes for docker compose & project build settings.

--- HandBrake-daemon/dockerInit.sh
+++ HandBrake-daemon/dockerInit.sh
@@ -2,7 +2,7 @@
 mountdir=/mnt/handbrake
 settings="$mountdir"/appsettings.json
 echo Initializing docker container
-if [[ ! -f /etc/"$conf" ]]; then #This is first run.
+if [[ ! -f /etc/handbrake-daemon.conf ]]; then #This is first run.
     echo Initialized watcher
     if [[ ! -f "$mountdir"/handbrake-daemon.conf ]]; then #Init was not called before, therefore we use blank config.
         cp default.conf "$mountdir"/handbrake-daemon.conf
@@ -10,10 +10,11 @@ if [[ ! -f /etc/"$conf" ]]; then #This is first run.
         mkdir "$mountdir"/sourceDirectory "$mountdir"/completedTranscodes "$mountdir"/originalMedia
     fi
 fi
-cp "$mountdir"/handbrake-daemon.conf /etc/handbrake-daemon.conf #Move the hostfile to take effect inside the container.
 if [[ ! -f "$settings" ]]; then
     echo Initialized appsettings
     cp appsettings.json "$settings"
 fi
+cp "$mountdir"/handbrake-daemon.conf /etc/handbrake-daemon.conf #Move the hostfile to take effect inside the container.
 cp "$settings" appsettings.json #Move the hostfile to take effect inside the container.
-dotnet handbrake-daemon.dll
+echo "Applied users mounted configurations."
+#dotnet handbrake-daemon.dll
